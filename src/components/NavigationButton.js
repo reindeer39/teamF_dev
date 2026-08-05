c239d8ee053c2808ab7d89c0812351cb5b73b811
YutoSato1412 <yutowing4869@gmail.com>
2026-08-05 10:40:24 +0900

    ナビゲーションボタンの変数追加

--- src/components/NavigationButton.js
+++ src/components/NavigationButton.js
@@ -1,8 +1,27 @@
 import './NavigationButton.css';
 
-function NavigationButton({ children, onClick }) {
+function NavigationButton({
+  children,
+  onClick,
+  width = null,
+  height = null,
+  backgroundColor = null,
+  hoverColor = null,
+  textColor = null,
+}) {
   return (
-    <button className="navigation-button" type="button" onClick={onClick}>
+    <button
+      className="navigation-button"
+      type="button"
+      onClick={onClick}
+      style={{
+        width,
+        height,
+        '--button-background-color': backgroundColor,
+        '--button-hover-color': hoverColor,
+        '--button-text-color': textColor,
+      }}
+    >
       {children}
     </button>
   );
