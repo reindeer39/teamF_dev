b40b1f25aa8ced51665235b1df7bbfe5d8f8b273
reindeer39 <kanaeruhito12@gmail.com>
2026-08-07 12:13:53 +0900

    fix: メンバーのデータを追加.

--- src/userIcons.js
+++ src/userIcons.js
@@ -1,24 +1,27 @@
-import avatar01 from './images/avatars/avatar-01.png';
-import avatar02 from './images/avatars/avatar-02.png';
-import avatar03 from './images/avatars/avatar-03.png';
-import avatar04 from './images/avatars/avatar-04.png';
-import avatar05 from './images/avatars/avatar-05.png';
 import avatarDefault from './images/avatars/avatar-default.png';
 
-const USER_ICONS = {
-  'avatar-01.png': avatar01,
-  'avatar-02.png': avatar02,
-  'avatar-03.png': avatar03,
-  'avatar-04.png': avatar04,
-  'avatar-05.png': avatar05,
-};
+const PRIVATE_MODE =
+  process.env.REACT_APP_DATA_MODE === 'private';
+
+const ALLOWED_ICONS = new Set([
+  'avatar-01.png',
+  'avatar-02.png',
+  'avatar-03.png',
+  'avatar-04.png',
+  'avatar-05.png',
+]);
 
 export function resolveUserIcon(iconName) {
-  if (!iconName) return avatarDefault;
+  if (!PRIVATE_MODE || !iconName) {
+    return avatarDefault;
+  }
 
-  // 将来APIがパスを返してもファイル名で解決できる
   const normalizedPath = iconName.replaceAll('\\', '/');
   const fileName = normalizedPath.split('/').pop();
 
-  return USER_ICONS[fileName] ?? avatarDefault;
+  if (!ALLOWED_ICONS.has(fileName)) {
+    return avatarDefault;
+  }
+
+  return `${process.env.PUBLIC_URL}/private-avatars/${fileName}`;
 }
